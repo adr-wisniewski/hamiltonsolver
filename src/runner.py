d36c0f6cec93d399ae31dd465407f1bbaf1471b7
sh <sh@localhost>
2011-01-08 13:24:32 +0100

    runner launch solver in increment seed mode

--- src/runner.py
+++ src/runner.py
@@ -4,4 +4,4 @@ algorithms = ['ge','gv','fs']
 for a in algorithms:
     print a
     for v in vertices:
-        os.system("main.py -r {0} {1} -d vst -l 10".format(v,a))
+        os.system("main.py -r {0} {1} -d vst -l 10 -ism".format(v,a))
